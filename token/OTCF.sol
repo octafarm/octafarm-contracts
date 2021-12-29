a04898bc63f57717d037e5c6efa002b204aa32e4
Ronan <96383475+furiousturt@users.noreply.github.com>
2021-12-30 00:17:36 +0400

    Adding token recovery

--- token/OTCF.sol
+++ token/OTCF.sol
@@ -2,13 +2,23 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract OTCF is ERC20 {
+contract OTCF is ERC20, Ownable {
+    using SafeERC20 for IERC20;
+
     constructor(
-        uint256 _initialSupply, 
         string memory _name, 
-        string memory _symbol
+        string memory _symbol,
+        uint256 _initialSupply
     ) public ERC20(_name, _symbol) {
-        _mint(msg.sender, _initialSupply);
-    }  
+        _mint(msg.sender, _initialSupply * (10 ** 18) );
+    } 
+
+    function clearTokens(address _token, uint256 _amount) external onlyOwner {
+        require(_token != address(this), "Cannot clear same tokens as OTCF");
+        IERC20(_token).safeTransfer(msg.sender, _amount);
+    }
 }
